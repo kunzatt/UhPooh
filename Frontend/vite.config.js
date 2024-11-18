840c4a8c461186630e90ff772fcef13f5054f14e
harimau97 <harimauho@gmail.com>
2024-11-18 15:01:28 +0900

    frontend

---
+++ Frontend/vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import veauryVitePlugins from "veaury/vite/index.js";
+export default defineConfig({
+  plugins: [
+    //replaces the vite() plugin
+    veauryVitePlugins({
+      type: "vue",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
